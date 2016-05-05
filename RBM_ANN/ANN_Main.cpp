8783f3899b7f51e97b46a325b11a9e3654ebd25d
webary <webary@sina.com>
2016-05-05 23:03:12 +0800

    define struct of training set

--- RBM_ANN/ANN_Main.cpp
+++ RBM_ANN/ANN_Main.cpp
@@ -2,11 +2,26 @@
 #include <iostream>
 using namespace std;
 
+typedef struct {
+    string file; //训练集文件
+    int n_out;   //输出层个数
+    int n_train; //训练集个数
+} TrainTxt;
+
+TrainTxt tt__[] = {
+    { "./train_test/train01.txt", 2, 300},  //[0]
+    { "./train_test/train0123.txt", 4, 600},   //[1]
+    { "./train_test/train012345.txt", 6, 900},    //[2]
+    { "./train_test/train01234567.txt", 8, 1200},   //[3]
+    { "./train_test/train0123456789.txt", 10, 1500},   //[4]
+};
+
 int main()
 {
     try {
-        ANN ann(784, 4, 1); //输入数据维数, 输出层结点个数, 隐藏层层数
-        ann.loadTrainSet("./train_test/train0123.txt", 500);
+        TrainTxt& tt = tt__[2]; ///通过修改序号载入不同的训练集
+        ANN ann(784, tt.n_out, 1); //输入数据维数, 输出层结点个数, 隐藏层层数
+        ann.loadTrainSet(tt.file, tt.n_train);
         ann.train(0.01, 1000000);
     } catch (const logic_error& err) {
         cout << "\r---error:" << err.what() << endl;
--- RBM_ANN/RBM_ANN_Main.cpp
+++ RBM_ANN/RBM_ANN_Main.cpp
@@ -1,16 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "RBM.h"
 #include "ANN.h"
+#include "Math_Util.hpp"
 #include <iostream>
 using namespace std;
 
 typedef struct {
     string file; //训练集文件
-    int out; //输出层个数，即有2^out种输出
-    int num; //训练集个数
+    int n_out;   //输出层个数
+    int n_train; //训练集个数
 } TrainTxt;
 
-TrainTxt tt[] = {
+TrainTxt tt__[] = {
     { "./train_test/train01.txt", 1, 300},  //[0]
     { "./train_test/train0123.txt", 2, 600},   //[1]
     { "./train_test/train012345.txt", 3, 900},    //[2]
@@ -20,15 +21,15 @@ TrainTxt tt[] = {
 
 int main()
 {
-    cout << RBM::getDateTime() << endl;
+    cout << Math_Util::getDateTime() << endl;
     SetText(FG_HL | FG_G | FG_B);
     try {
-        TrainTxt &t = tt[2]; //通过修改序号载入不同的训练集
+        TrainTxt& tt = tt__[2]; //通过修改序号载入不同的训练集
         int hideUnits[] = { 144, 36 };
         RBM rbm(784, hideUnits);
         cout << "\r>>>正在载入训练数据和测试数据...";
         time_t t_start = clock();
-        rbm.loadTrain(t.file, t.num);
+        rbm.loadTrain(tt.file, tt.n_train);
         cout << "\r载入训练和测试数据耗时: " << (clock() - t_start) << "ms" << endl;
         t_start = clock();
         rbm.train(0.05, 10000);  //允许误差和最大代数，任意一个满足则停止
@@ -36,7 +37,7 @@ int main()
         rbm.saveRBMOutToFile("rbmOut.txt"); //将RBM的输出层以及标签保存到文件
         uint rbmOutSize = hideUnits[sizeof(hideUnits) / sizeof(hideUnits[0]) - 1];
         ///将RBM抽取的特征送到ANN中进行分类
-        ANN ann(rbmOutSize, t.out, 1); //输入数据大小，输出层神经元个数，隐层层数
+        ANN ann(rbmOutSize, tt.n_out, 1); //输入数据大小,输出层神经元个数,隐层层数
         ann.loadTrainSet("rbmOut.txt"); //读取RBM的输出作为ANN的训练数据
         ann.train(0.02, 1000000);
     } catch (const logic_error &err) {
